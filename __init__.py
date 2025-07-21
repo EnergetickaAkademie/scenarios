4164853ac60e2ca8a06071b4df99bbd36a25bbe2
Jakub Pelc <jakub.pelc@seznam.cz>
2025-07-21 18:39:33 +0200

    created init file

---
+++ __init__.py
@@ -0,0 +1,53 @@
+from .Enak import (
+	Source,
+	RoundType,
+	WeatherType,
+	Building,
+
+	CITY_CENTERS,
+
+	Round,
+	PlayRound,
+
+	Day,
+	Night,
+	Slide,
+	SlideRange,
+
+	WeatherRound,
+	Windy,
+	Rainy,
+	Sunny,
+	Cloudy,
+	Foggy,
+	Snowy,
+
+	Script
+)
+
+__all__ = [
+	'Source',
+	'RoundType',
+	'WeatherType',
+	'Building',
+
+	'CITY_CENTERS',
+
+	'Round',
+	'PlayRound',
+
+	'Day',
+	'Night',
+	'Slide',
+	'SlideRange',
+
+	'WeatherRound',
+	'Windy',
+	'Rainy',
+	'Sunny',
+	'Cloudy',
+	'Foggy',
+	'Snowy',
+
+	'Script'
+]
